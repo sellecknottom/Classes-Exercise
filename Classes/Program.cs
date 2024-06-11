c9406e46811156a38cc5edc8123baebbe99c1cf1
Selleck Elliott <selleck.elliott@gmail.com>
2024-06-11 12:49:43 -0600

    create car class

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car() {Make = "Buick", Model = "Enclave", Year = "2024"};
+            Console.WriteLine($"{car1.Make} {car1.Model} {car1.Year}");
         }
     }
 }
